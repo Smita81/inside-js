7e7528e49b54795186a849997ec01965849d84a8
smita81 <smitabbsaraswat@gmail.com>
2024-03-12 20:58:52 +0100

    reordering of lesson plan * reordered weekly plans
    * reordered suggested lesson plan
    
    * lesson plan changed by removing unit tests section
    
    <!--
      make this PR easy to find:
    
      - assign and add reviewers
      - add any helpful labels
      - connect it to a milestone (if necessary)
      - link it with an issue (if necessary)
    -->
    
    <!-- describe your PR -->
    
    # Description
    
    <!-- A clear and concise description of what the Pull Request is about. -->
    
    ## Checklists
    
    ### General Checks
    
    - [ ] the branch is up to date with `main`/`master`
    - [ ] the code works when pulled and run locally
    - [ ] All CI checks pass
    - [ ] all conflicts are resolved (if any)
    - [ ] PR has a descriptive title
    - [ ] PR has appropriate labels and milestones for easy identification
    - [ ] PR it is assigned to the owner
    - [ ] reviewers are assigned
    - [ ] the PR contributes only one focused change
    - [ ] It is in the appropriate column in the project board (if necessary)
    - [ ] has short and clear description
    - [ ] is linked to an issue (if it is related)
    - [ ] feedback is addressed (if any and if it is appropriate feedback.)
    
    ## Markdown
    
    <!-- markdown-specific checks -->
    
    - [ ] the markdown source is formatted
    - [ ] spelling and grammar is correct in all text
    - [ ] The markdown looks correct when you preview the file
    - [ ] all links and images work

--- lesson-plans/for-loops/2-integrate/repeat-characters/script.js
+++ lesson-plans/for-loops/2-integrate/repeat-characters/script.js
@@ -1,15 +1,20 @@
 import { readNumber, readString, display } from '../../../../lib/dom-io.js';
 
-document.getElementById(__).addEventListener('click', () => {
+document.getElementById("repeat-them").addEventListener('click', () => {
   debugger;
 
-  // read user values
+  // read user values smith
+  let text = readString ("user-text");
+  // read user number 2/3
+  let number = readNumber ("number-of-times");
 
   // repeat the characters in the text
 
   for (_; _; _) {
-    for (_; _; _) {}
+    for (_; _; _) { }
   }
 
   // display the text with repeated characters
+  let output = "repeated-output"
+
 });
