4197df0b33d7d831b1c5432ded5799214186db67
smita81 <smitabbsaraswat@gmail.com>
2024-03-12 22:20:32 +0100

    addrepeatcharacters

--- lesson-plans/for-loops/2-integrate/repeat-characters/script.js
+++ lesson-plans/for-loops/2-integrate/repeat-characters/script.js
@@ -3,18 +3,26 @@ import { readNumber, readString, display } from '../../../../lib/dom-io.js';
 document.getElementById("repeat-them").addEventListener('click', () => {
   debugger;
 
-  // read user values smith
-  let text = readString ("user-text");
+  // read user values
+
+  let text = readString('user-text')
+ 
   // read user number 2/3
+ 
   let number = readNumber ("number-of-times");
 
   // repeat the characters in the text
 
-  for (_; _; _) {
-    for (_; _; _) { }
+ let newText = '';
+  for (let i = 0; i < text.length; i++) {
+    for (let d = 0; d < repetationNumbers; d++) {
+      newText += text[i]; 
+      
+      }
   }
 
   // display the text with repeated characters
-  let output = "repeated-output"
+ 
+  output("repeated-output", newText);
 
 });
